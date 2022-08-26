e1e239e3586a0ac72b508cbc0565e473b3d2ee43
Nazar <nazarlykashik@gmail.com>
2022-08-26 18:41:24 +0300

    add alamofire at one green

--- Test 1/2 example/Model.swift
+++ Test 1/2 example/Model.swift
@@ -36,8 +36,32 @@ struct Plases: Decodable{
     let photo: String?
     let city_id: Int?
     let visible: Bool?
-//    let tags: Any?
     let is_excursion: Bool?
+    
+    
+    init(dictPlase: [String: Any]){
+        index = dictPlase["index"] as? Int
+        id = dictPlase["id"] as? Int
+        id_point = dictPlase["id_point"] as? Int
+        name = dictPlase["name"] as? String
+        text = dictPlase["text"] as? String
+        sound = dictPlase["sound"] as? String
+        lang = dictPlase["lang"] as? Int
+        last_edit_time = dictPlase["last_edit_time"] as? Int
+        creation_date = dictPlase["creation_date"] as? String
+        lat = dictPlase["lat"] as? Float
+        lng = dictPlase["lng"] as? Float
+        logo = dictPlase["logo"] as? String
+        photo = dictPlase["photo"] as? String
+        city_id = dictPlase["city_id"] as? Int
+        visible = dictPlase["visible"] as? Bool
+        is_excursion = dictPlase["is_excursion"] as? Bool
+    }
+    
+    static func getPlases(from jsonData: Any) -> [Plases]{
+        guard let jsonData = jsonData as? Array <[String: Any]> else {return []}
+        return jsonData.compactMap{ Plases(dictPlase: $0) }
+    }
 }
 
 
--- Test 1/2 example/PlasesViewController.swift
+++ Test 1/2 example/PlasesViewController.swift
@@ -6,13 +6,13 @@
 //
 
 import UIKit
+import Alamofire
 
 class PlasesViewController: UITableViewController {
     
     @IBOutlet var DetailTableView: UITableView!
     
     let jsonOfPlases = "https://krokapp.by/api/get_points/11/"
-    var places: [Plases] = []
     var placesRu: [Plases] = []
     var cityId = 0
 
@@ -21,7 +21,6 @@ class PlasesViewController: UITableViewController {
         super.viewDidLoad()
         fetchPlases()
         print("айдишка\(cityId)")
-        
     }
 
     // MARK: - Table view data source
@@ -60,43 +59,6 @@ class PlasesViewController: UITableViewController {
         return cell
     }
 
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -104,29 +66,20 @@ class PlasesViewController: UITableViewController {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
+    
     func fetchPlases(){
         guard let url = URL(string: jsonOfPlases) else {return}
-        URLSession.shared.dataTask(with: url) { [self] (data, _, error) in
-        
-            if let error = error {
-                print(error.localizedDescription)
-                return
-            }
-            guard let data = data else {return}
-            do {
-                self.places = try JSONDecoder().decode([Plases].self, from: data)
-                let lang = places.filter { $0.lang == 3 && $0.name != "" && $0.city_id == cityId }
-                placesRu = lang
-                print(placesRu)
-
+        AF.request(url).validate().responseJSON { dataResponse in
+            switch dataResponse.result{
+            case .success(let value):
+                self.placesRu = Plases.getPlases(from: value).filter { $0.lang == 3 && $0.name != "" && $0.city_id == self.cityId }
+                print(self.placesRu)
                 DispatchQueue.main.async {
                     self.tableView.reloadData()
                 }
-            } catch let error{
+            case .failure(let error):
                 print(error)
             }
-        }.resume()
+        }
     }
-    
 }
