5767dae98e25ae4a0d3ac758b6be1e2800088c61
Nazar <nazarlykashik@gmail.com>
2022-09-02 14:33:46 +0300

    1 exercise done

--- Test 1/Map 1 example/Points.swift
+++ Test 1/Map 1 example/Points.swift
@@ -7,6 +7,50 @@
 
 import Foundation
 
-class Poit{
-
+struct Points: Decodable{
+    let id : Int?
+    let lang_id : Int?
+    let visible : Bool?
+    let point_name : String?
+    let point: BodyOfPoint?
+    
+    init (dictPoint: [String: Any]){
+        id = dictPoint["id"] as? Int
+        lang_id = dictPoint["lang_id"] as? Int
+        visible = dictPoint["visible"] as? Bool
+        point_name = dictPoint["point_name"] as? String
+        point = BodyOfPoint.getBody(from: dictPoint["point"] as Any)
+    }
+    static func getPoinst(from jsonData: Any) -> [Points]{
+        guard let jsonData = jsonData as? Array <[String: Any]> else {return []}
+        return jsonData.compactMap{ Points(dictPoint: $0) }
+    }
 }
+
+struct BodyOfPoint: Decodable{
+    let id : Int?
+    let point_district_id : Int?
+    let water_object_id : Int?
+    let point_geo_type : String?
+    let lat : Double
+    let lng : Double
+    let lat_2 : Double?
+    let lng_2 : Double?
+    
+    init (dictBody: [String: Any]){
+        id = dictBody["id"] as? Int
+        point_district_id = dictBody["point_district_id"] as? Int
+        water_object_id = dictBody["water_object_id"] as? Int
+        point_geo_type = dictBody["point_geo_type"] as? String
+        lat = dictBody["lat"] as? Double ?? 50
+        lng = dictBody["lng"] as? Double ?? 30
+        lat_2 = dictBody["lat_2"] as? Double
+        lng_2 = dictBody["lng_2"] as? Double
+    }
+    static func getBody(from jsonData: Any) -> BodyOfPoint?{
+        guard let jsonData = jsonData as? [String: Any] else {return nil}
+        return BodyOfPoint(dictBody: jsonData)
+    }
+}
+
+
--- Test 1/Map 1 example/Test 1/MapController.swift
+++ Test 1/Map 1 example/Test 1/MapController.swift
@@ -6,34 +6,93 @@
 //
 
 import UIKit
+import Alamofire
 
 class MapController: UIViewController {
     @IBOutlet var map: GMSMapView!
+    
+    let pointsUrl = "https://fish-pits.krokam.by/api/rest/points/?format=json"
+    var points: [Points] = []
+    
     
     // 1) create class Point
     // 2) Point[] = []
     // 3) create [] GMSMarker[] title === pointname
     
     
-//    let markers = [ {lang: 54, lat: 27}, {lang:56, lat: 26} ]
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let position = CLLocationCoordinate2D(latitude: 53, longitude: 27)
-        let marker = GMSMarker(position: position)
-        marker.title = "Hello World"
-        marker.map = map
+        fetchPoints()
+ 
  }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func fetchPoints(){
+        guard let url = URL(string: pointsUrl) else {return}
+        AF.request(url).validate().responseJSON { dataResponse in
+            switch dataResponse.result{
+            case .success(let value):
+                //print(value)
+                self.points = Points.getPoinst(from: value)//.filter { $0.lang_id == 3 && $0.point_name != "" }
+                self.viewDidLoad()
+                print(self.points)
+                for point in self.points {
+                    
+                    let position = CLLocationCoordinate2D(latitude: CLLocationDegrees(Float(point.point!.lat)), longitude: CLLocationDegrees(Float(point.point!.lng)))
+                    let marker = GMSMarker(position: position)
+                    marker.title = point.point_name
+                    marker.map = self.map
+                    
+                    //print(point)
+                }
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
-    */
-
 }
+//    // MARK: - CLLocationManagerDelegate
+//    //1
+//    extension MapViewController: CLLocationManagerDelegate {
+//      // 2
+//      func locationManager(
+//        _ manager: CLLocationManager,
+//        didChangeAuthorization status: CLAuthorizationStatus
+//      ) {
+//        // 3
+//        guard status == .authorizedWhenInUse else {
+//          return
+//        }
+//        // 4
+//        locationManager.requestLocation()
+//
+//        //5
+//        mapView.isMyLocationEnabled = true
+//        mapView.settings.myLocationButton = true
+//      }
+//
+//      // 6
+//      func locationManager(
+//        _ manager: CLLocationManager,
+//        didUpdateLocations locations: [CLLocation]) {
+//        guard let location = locations.first else {
+//          return
+//        }
+//
+//        // 7
+//        mapView.camera = GMSCameraPosition(
+//          target: location.coordinate,
+//          zoom: 15,
+//          bearing: 0,
+//          viewingAngle: 0)
+//      }
+//
+//      // 8
+//      func locationManager(
+//        _ manager: CLLocationManager,
+//        didFailWithError error: Error
+//      ) {
+//        print(error)
+//      }
+//    }
+
+
